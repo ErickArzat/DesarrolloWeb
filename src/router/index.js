f8e1b752b23b6b66974552e0355db72be51fdf6a
MonicaGarcilazo <mgarcilazo@walook.com.mx>
2023-12-03 07:40:37 -0600

    Register view

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,17 @@ const router = createRouter({
   routes: [
     
     {
-      path: '/Login',
+      path: '/login',
       name: 'login',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/LogIn.vue')
+    }, 
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/Register.vue')
     }, 
     {
       path: '/home',
